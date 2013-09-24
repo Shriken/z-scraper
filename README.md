af5794b6dceb061bc599ed801cc6a748bd8348b4
Zane Sterling <sterling.zane@gmail.com>
2013-09-24 19:57:07 -0400

    Add project.

---
+++ README.md
@@ -0,0 +1,1 @@
+A tool to scrape stuycs-softdev.github.io (Mr Zamansky's Softdev site) for new homework posts.
---
+++ scraper.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+import requests
+import os
+
+r = False
+
+while not r:
+	r = requests.get('http://stuycs-softdev.github.io/assignments.html')
+
+t = r.text
+t = t.replace('&#8217;', "'")
+t = t[t.find('<h1>Assignments</h1>'):]
+t = t[:t.find('</div>') - 9]
+
+def numAssignments(page):
+	count = 0
+	flag = False
+
+	while not flag:
+		findString = 'hw-%i-github.html'%(count+1)
+		index = page.find(findString)
+		flag = (index == -1)
+
+		if not flag:
+			count += 1
+
+	return count
+
+lastCount = 0
+
+if 'lastCount' in os.listdir('.'):
+	f = open('lastCount')
+	text = f.read()
+
+	lastCount = int(text)
+else:
+	f = open('lastCount', 'w')
+
+	lastCount = 0
+	f.write(str(lastCount))
+
+count = numAssignments(t)
+
+if lastCount == count:
+	print "Nothing new. Carry on jubilantly."
+else:
+	print "There's a new assignment! Check out the page."
+
+	f = open('lastCount', 'w')
+	f.write(str(count));
+
